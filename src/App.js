b822e59c31ab30f6c6e909961efd6a23a0b80cbf
paragbadgujar1205 <paragbadgujar1205@gmail.com>
2024-05-01 17:30:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import Navbar from "./Components/Navbar/Navbar.jsx";
+import Background from "./Components/Background/Background.jsx";
+import Hero from './Components/Hero/Hero.jsx';
 
-function App() {
+const App = () => {
+  const [heroCount,setHeroCount]= useState(2);
+  const [playStatus,setPlayStatus]=useState(false);
+
+  useEffect(()=>{
+    setInterval(()=>{
+      setHeroCount((count)=>{return count===2?0:count+1})
+    }, 5000)
+  },[])
+
+  let heroData = [
+    {text1:"Dive into",text2:"what you love"},
+    {text1:"Indulge",text2:"your passions"},
+    {text1:"Give in to",text2:"Your passions"},
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Background playStatus={playStatus} heroCount={heroCount}/>
+      <Navbar/>
+      <Hero setPlayStatus={setPlayStatus} heroData={heroData[heroCount]} heroCount={heroCount} setHeroCount={setHeroCount} playStatus={playStatus} />
+    </>
+  )
 }
 
-export default App;
+export default App
